de63096dd777132099fe4688340973b4a0c45e4f
walthercio Almeida <suaagenciainicial@gmail.com>
2022-09-10 19:00:06 -0300

    add components and page

---
+++ meusite/src/components/section.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Section extends React.Component {
+    render() {
+        return (
+            <h1>Section</h1>
+        )
+    }
+}
+
+export default Section;
